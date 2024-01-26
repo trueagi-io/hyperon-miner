f8af74970848dbe94a9ab4ef6432b36700c37ac0
Hedra <tanksh24@gmail.com>
2024-01-25 19:32:08 -0500

    updated utils

--- utils/helper.py
+++ utils/helper.py
@@ -4,10 +4,10 @@ from hyperon import *
 @register_atoms
 def count_atoms():
     count = OperationAtom(
-        'TupleCount', 
+        'tuple-count', 
         lambda atom: [ValueAtom(len(atom.get_children()), 'Number')], [AtomType.ATOM, "Number"], 
         unwrap=False)
     return {
-        r"TupleCount": count
+        r"tuple-count": count
     }
  
